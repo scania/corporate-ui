3322f85826cd287f4551a0e64cb9aae7726a0883
Andreas Wikström <andreas.wikstrom@scania.com>
2017-06-08 10:57:06 +0200

    Fix for sticky navigation - Changed main-navigation to template to work with new strucure

--- src/views/component/Navigation/main-navigation/script.ts
+++ src/views/component/Navigation/main-navigation/script.ts
@@ -34,33 +34,12 @@ Polymer({
 
     // Set start collapse value - couldnt get this to work in a better way...
     $('.navbar-toggle > a', this).addClass('collapsed');
-
-    // Handling for navigation scoll in mobile mode
-    /*$('#main-nav', this).on('shown.bs.collapse hide.bs.collapse', function(event) {
-      $('body').toggleClass('navigation-open');
-    });*/
-
-    // If no nav-item is selected set top-level as selected
-    /*if( !$('nav-item.active', this).length ) {
-      $('.top-level').addClass('active');
-    } */
-
-    // This have been moved to css section, should work
-    /*$('main-navigation .nav-item').bind("mouseover", function(){
-      var color  = $(this).css("color");
-      $('primary-items .nav-item').css("color", "#d4d4d3");
-      $('primary-items .nav-item:hover').css("color", "#041e42");
-
-      $(this).bind("mouseout", function(){
-        $('primary-items .nav-item').css("color", "#041e42");
-      })
-    });*/
   },
   // if we've scrolled more than the navigation, change its position to fixed making it stick to the top,
   // otherwise change it back to relative
   sticky: function() {
     var body = $('body'),
-        navContainer = $('.nav-container', this),
+        navContainer = $('> .navbar-default', this),
         stickyNavTop = $(this).offset().top,
         scrollTop = $(window).scrollTop(), // our current vertical position from the top
         headerHeight = $('.navbar-toggle:visible', this).height() || $('#main-nav .top-level', this).height() + $('sub-navigation:visible', this).height() || 'auto',// On desktop mode it will use #main-nav on mobile .navbar-toggle

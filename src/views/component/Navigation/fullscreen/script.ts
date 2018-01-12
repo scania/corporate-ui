f443c12dd749ce012f0ba9d393b1a396bda30a21
GLOBAL\awinny <andreas.wikstrom@scania.com>
2018-01-12 12:46:58 +0100

    We want to be able to toggle fullscreen globally - Added listener for global event - Changed event name from toggle-fullscreen to fullscreen-toggled - Added gloably success event frigger

--- src/views/component/Navigation/fullscreen/script.ts
+++ src/views/component/Navigation/fullscreen/script.ts
@@ -11,6 +11,9 @@ Polymer({
       value: 'fullscreen'
     }
   },
+  registered: function() {
+    document.addEventListener('toggle-fullscreen', this.toggleFullscreen.bind(this), false);
+  },
   attached: function() {
     var mode = sessionStorage.getItem(this.name);
     if(mode) {
@@ -18,6 +21,7 @@ Polymer({
     }
   },
   toggleFullscreen: function(e) {
+    var triggeredGlobally = !this._readied;
     e.preventDefault();
     document.body.classList.toggle(this.name);
     sessionStorage.setItem(this.name, 'true');
@@ -26,8 +30,16 @@ Polymer({
       sessionStorage.removeItem(this.name);
     }
 
-    this.async(function() {
-      this.fire('toggle-fullscreen');
-    });
+    if (!triggeredGlobally) {
+      // This will bubble from current node and down, but if this happens 
+      // from outside we should only trigger the event globally
+      this.async(function() {
+        this.fire('fullscreen-toggled');
+      });
+    } else {
+      var newEvent = document.createEvent('Event');
+      newEvent.initEvent('fullscreen-toggled', true, true);
+      document.dispatchEvent(newEvent);
+    }
   }
 });

474256ea812d6a5e380cbe35fa08f1942290731c
Andreas Wikström <andreas.wikstrom@scania.com>
2018-09-05 13:34:30 +0200

    Correction for content positioning in mobile mode - Fix for mobile condition to include 991 and not just lower then 991

--- src/components/corporate-header/script.ts
+++ src/components/corporate-header/script.ts
@@ -64,7 +64,7 @@ Polymer({
   },
   setSize: function() {
     this.style.padding = '';
-    if(window.innerWidth < 991) {
+    if(window.innerWidth <= 991) {
       // TODO - We should use height, but then we need to add flex-grow & flex-shrink
       this.style.paddingTop = this.querySelector('.navbar-default').offsetHeight + 'px';
     }

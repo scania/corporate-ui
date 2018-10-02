c15cb400aa6d5992bd10bb1a4977f604fb494301
EYUZ78 <ela.yudhanira@scania.com>
2018-10-02 14:45:56 +0200

    Add multiple attribute and upload button

--- src/components/file-upload/script.ts
+++ src/components/file-upload/script.ts
@@ -11,12 +11,24 @@ Polymer({
     filenames: {
       type: Array,
       value:[]
+    },
+    _isFiles: {
+      type: Boolean,
+      value: false
+    },
+    multiple: {
+      type: Boolean,
+      value: false
     }
   },
   handleChange: function(e){
+    if(this.multiple==false){
+      this.filenames=[];
+    }
     var files = e.srcElement.files;
     for(var i=0; i<files.length; i++){
       this.push('filenames',{name: files[i].name});
     }
+    if(this.filenames!=[]) this._isFiles=true;
   }
 });

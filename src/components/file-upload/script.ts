d64a3d27674b4396445944b8127c623a6e5801b2
EYUZ78 <ela.yudhanira@scania.com>
2018-10-04 11:56:52 +0200

    Copy file array into fileList and sort filenames in the DOM

--- src/components/file-upload/script.ts
+++ src/components/file-upload/script.ts
@@ -8,9 +8,9 @@ Polymer({
     name: {
       type: String
     },
-    filenames: {
+    fileList: {
       type: Array,
-      value:[]
+      value: []
     },
     _isFiles: {
       type: Boolean,
@@ -19,21 +19,59 @@ Polymer({
     multiple: {
       type: Boolean,
       value: false
+    },
+    itemKey: {
+      type: Number
+    },
+    fileIdCounter: {
+      type: Number,
+      value: 0
     }
   },
   handleChange: function(e){
     if(this.multiple==false){
-      this.filenames=[];
+      this.fileList=[];
     }
     var files = e.srcElement.files;
     var filesize = 0;
     for(var i=0; i<files.length; i++){
       filesize = Math.round(files[i].size/1024);
-      this.push('filenames',{
-        name: files[i].name,
-        size: filesize
+      this.fileIdCounter++;
+      this.push('fileList',{
+        file: files[i],
+        size: filesize,
+        id: this.fileIdCounter
       });
     }
-    if(this.filenames!=[]) this._isFiles=true;
+    this.updateIsFiles();
+    console.log(this.fileList);
+  },
+  removeFile: function(e){
+    e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+    var fileId = e.target.id;
+
+    for(var j=0; j<this.fileList.length; j++){
+      if(this.fileList[j].id == fileId){
+        this.fileList.splice(j,1);
+      }
+
+    }
+    console.log(e);
+    this.updateIsFiles();
+  },
+  updateIsFiles: function(){
+    this._isFiles = (this.fileList.length!=0) ? true : false;
+  },
+  sortFilenames: function(a,b){
+    var aId = a.id;
+    var bId = b.id;
+
+    if(aId < bId){
+      return -1;
+    }
+    if(aId > bId){
+      return 1;
+    }
+    return 0;
   }
 });

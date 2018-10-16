18fdd133bf50b36dcc71b91bdb54d94c5f2b0d61
MMEXVR <Marcus.mena.pacheco@scania.com>
2018-10-16 15:27:09 +0200

    Cookie component dependecies added - Upgraded from 2.1.4 - types@js-cookie - Js-cookie 2.2.0

--- src/components/cookie-message/script.ts
+++ src/components/cookie-message/script.ts
@@ -1,10 +1,27 @@
 Polymer({
   is: name,
   properties: {
-    title: String,
-    text: String
+    text: String,
+    expire: Number
   },
   ready: function(){
+    var Cookie = this;
+    var CookieMessage = Cookie.text
+    var CookieElm = document.querySelector("c-cookie-message")
+    var CookieClose = CookieElm.querySelector(".close-cookie")
 
+    this._CookieCheckFunction(CookieClose, CookieMessage, Cookie);
+  },
+  _CookieCheckFunction: function(CookieClose, CookieMessage, Cookie) {
+    CookieClose.addEventListener('click', function(CookieMessage){
+      // Add Cookie
+      Cookie._CookieAddFunction(CookieMessage, Cookie);
+    });
+  },
+  _CookieAddFunction: function(CookieMessage, Cookie) {
+
+    // Sets Cookie on click
+    Cookies.set('Scania-Cookie', CookieMessage, {expires: this.expire || 900, path: ''});
+    Cookie.style.display = "none";
   }
 });

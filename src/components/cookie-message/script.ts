0173b07e2cd89e3fca1bc00dac5b0b761cd1354f
MMEXVR <Marcus.mena.pacheco@scania.com>
2018-10-22 13:20:31 +0200

    Added content tag within c-corporate-cookie instead of attribute for the value of the cookie message

--- src/components/cookie-message/script.ts
+++ src/components/cookie-message/script.ts
@@ -1,27 +1,35 @@
 Polymer({
   is: name,
   properties: {
+    name: String,
     text: String,
-    expire: Number
+    expire: Number,
+    lang: String
   },
-  ready: function(){
-    var Cookie = this;
-    var CookieMessage = Cookie.text
-    var CookieElm = document.querySelector("c-cookie-message")
-    var CookieClose = CookieElm.querySelector(".close-cookie")
+  attached: function(){
+    var Cookie = this,
+      CookieMessage = Cookie.text || Cookie.querySelector('.c-cookie-message').innerText,
+      CookieElm = document.querySelector("c-cookie-message"),
+      CookieClose = CookieElm.querySelector(".close-cookie");
 
     this._CookieCheckFunction(CookieClose, CookieMessage, Cookie);
   },
   _CookieCheckFunction: function(CookieClose, CookieMessage, Cookie) {
-    CookieClose.addEventListener('click', function(CookieMessage){
+
+    console.log(Cookies.get("Scania-Cookie"))
+
+    CookieClose.addEventListener('click', function(){
       // Add Cookie
       Cookie._CookieAddFunction(CookieMessage, Cookie);
     });
   },
   _CookieAddFunction: function(CookieMessage, Cookie) {
 
-    // Sets Cookie on click
+    // Sets Cookie using https://github.com/js-cookie/js-cookie/tree/v2.2.0
     Cookies.set('Scania-Cookie', CookieMessage, {expires: this.expire || 900, path: ''});
     Cookie.style.display = "none";
+  },
+  _CookieRemoveFunction: function(){
+
   }
 });

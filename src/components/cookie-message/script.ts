8afc928ea25f4f23954adfa9feca31eb2fae69eb
MMEXVR <Marcus.mena.pacheco@scania.com>
2018-10-22 15:03:27 +0200

    Cookie-message component - Added check for old cookie

--- src/components/cookie-message/script.ts
+++ src/components/cookie-message/script.ts
@@ -3,33 +3,31 @@ Polymer({
   properties: {
     name: String,
     text: String,
-    expire: Number,
-    lang: String
+    expire: Number
   },
   attached: function(){
+    /* Sets Cookie using https://github.com/js-cookie/js-cookie/tree/v2.2.0 */
     var Cookie = this,
       CookieMessage = Cookie.text || Cookie.querySelector('.c-cookie-message').innerText,
       CookieElm = document.querySelector("c-cookie-message"),
       CookieClose = CookieElm.querySelector(".close-cookie");
 
-    this._CookieCheckFunction(CookieClose, CookieMessage, Cookie);
+      this._CookieCheckFunction(CookieClose, CookieMessage, Cookie);
   },
   _CookieCheckFunction: function(CookieClose, CookieMessage, Cookie) {
+    var oldCookie = Cookies.get(Cookie.name);
 
-    console.log(Cookies.get("Scania-Cookie"))
-
-    CookieClose.addEventListener('click', function(){
-      // Add Cookie
-      Cookie._CookieAddFunction(CookieMessage, Cookie);
-    });
+    // If cookie exist
+    if(CookieMessage != oldCookie) {
+      CookieClose.addEventListener('click', function(){
+        Cookie._CookieAddFunction(CookieMessage, Cookie);
+      });
+    } else {
+      Cookie.style.display = "none";
+    }
   },
   _CookieAddFunction: function(CookieMessage, Cookie) {
-
-    // Sets Cookie using https://github.com/js-cookie/js-cookie/tree/v2.2.0
-    Cookies.set('Scania-Cookie', CookieMessage, {expires: this.expire || 900, path: ''});
+    Cookies.set(Cookie.name , CookieMessage, {expires: this.expire || 900, path: ''});
     Cookie.style.display = "none";
-  },
-  _CookieRemoveFunction: function(){
-
   }
 });

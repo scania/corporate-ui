7e502a87c22c91a14ce0ea6a0ead10652a1f959d
awinny <andreas.wikstrom@scania.com>
2018-11-01 10:48:09 +0100

    Re add local vendor root - Re added removed vendor root script, this will be removed when creating the npm handling setup

--- server.js
+++ server.js
@@ -17,6 +17,18 @@ function server() {
   app.use(express.static(__dirname + '/demo'))
   app.use('/', express.static(__dirname + '/dist'))
 
+  app.use('/vendors/:type/:dependency/:version/*', function(req, res) {
+    path = req.params[0].replace('bootstrap-org', 'bootstrap')
+    dependency = req.params.dependency
+    if (!req.params.version.match(/\d.\d.\d/g)) {
+      path = path.substring(path.indexOf("/") + 1)
+      dependency = req.params.version
+    }
+    res.sendFile(__dirname + '/node_modules/' + dependency + '/' + path)
+  })
+
+  app.use('/resources/logotype/scania', express.static(__dirname + '/dist/images') )
+
   app.listen(app.get('port'), function() {
     console.log('UX-library is now running at http://%s:%d.', app.get('host'), app.get('port'))
   })

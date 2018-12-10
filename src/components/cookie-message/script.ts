57138fe422e652ea216f150ada8fb19d724d39a4
GLOBAL\mmexvr <marcus.mena.pacheco@scania.com>
2018-12-10 16:22:23 +0100

    Cookie-message component improvements - Modal accept button working - Added cookie-component example on panel site - Small styling (temporary)

--- src/components/cookie-message/script.ts
+++ src/components/cookie-message/script.ts
@@ -11,16 +11,16 @@ Polymer({
       observer: '_langSupport'
     }
   },
-  attached: function(){
+  ready: function(){
     /* Sets Cookie using https://github.com/js-cookie/js-cookie/tree/v2.2.0 */
     var Cookie = this,
       CookieMessage = Cookie.text || Cookie.querySelector('.c-cookie-message').innerText,
       CookieElm = document.querySelector("c-cookie-message"),
-      CookieClose = CookieElm.querySelector(".close-cookie");
-
-      this._CookieCheckFunction(CookieClose, CookieMessage, Cookie);
+      CookieClose = CookieElm.querySelector(".close-cookie"),
+      CookieClose2 = CookieElm.querySelector(".modal .close-cookie");
+      this._CookieCheckFunction(CookieClose, CookieClose2, CookieMessage, Cookie);
   },
-  _CookieCheckFunction: function(CookieClose, CookieMessage, Cookie) {
+  _CookieCheckFunction: function(CookieClose, CookieClose2, CookieMessage, Cookie) {
     var oldCookie = Cookies.get(Cookie.name);
 
     // If cookie exist
@@ -28,13 +28,16 @@ Polymer({
       CookieClose.addEventListener('click', function(){
         Cookie._CookieAddFunction(CookieMessage, Cookie);
       });
-    } else {
-      Cookie.style.display = "none";
+      CookieClose2.addEventListener('click', function(){
+        Cookie._CookieAddFunction(CookieMessage, Cookie);
+      });
+    } else if(this) {
+      Cookie.parentNode.removeChild(Cookie)
     }
   },
   _CookieAddFunction: function(CookieMessage, Cookie) {
     Cookies.set(Cookie.name , CookieMessage, {expires: Cookie.expire || 900, path: Cookie.path || '.scania.com'});
-    Cookie.style.display = "none";
+    Cookie.parentNode.removeChild(Cookie)
   },
   _langSupport: function(lang, string){
     // Sets the cookie to specific language to modal

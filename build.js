31d77accc7ebb7ec9fa02650dd9925e1d8d4cbbe
Andreas Wikström <andreas.wikstrom@scania.com>
2019-02-20 10:36:18 +0100

    Added watch task for build themes - Added a watch task for building themes

--- build.js
+++ build.js
@@ -1,84 +1,106 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
+
+const components = 'src/components/'
+const inputFolder = 'src/themes/'
+const outputFolder = 'src/themes.built/'
 
 function isInArray(value, array) {
-  return array.indexOf(value) > -1;
+  return array.indexOf(value) > -1
 }
 
-var walkDir = function(dir, done) {
-  var globalCSS =[], // save global CSS
-      componentCSS ={}, // save component css theme
-      data = {}, // temporary container for file content
-      cssContent=''; // temporary container for string addition
+function walkDir(dir, done) {
+  var globalCSS = [], // save global CSS
+    componentCSS = {}, // save component css theme
+    data = {}, // temporary container for file content
+    cssContent = '' // temporary container for string addition
   fs.readdir(dir, function(err, list) {
-    if (err) { return done(err);}
+    if (err) {
+      return done(err)
+    }
 
-    var pending = list.length;
-    if (!pending){ return done(null, data);}
+    var pending = list.length
+    if (!pending) {
+      return done(null, data)
+    }
     list.forEach(function(file) {
-      file = path.resolve(dir, file);
+      file = path.resolve(dir, file)
       fs.stat(file, function(err, stat) {
         if (stat && stat.isDirectory()) {
           walkDir(file, function(err, dt) {
-            for(var a in dt){
-              var filename = path.parse(a).name;
-              var brandName = path.basename(path.dirname(a));
-              if(!isInArray(brandName, globalCSS)){
-                globalCSS.push(brandName);
+            for (var a in dt) {
+              var filename = path.parse(a).name
+              var brandName = path.basename(path.dirname(a))
+              if (!isInArray(brandName, globalCSS)) {
+                globalCSS.push(brandName)
               }
 
-              cssContent = '';
-              cssContent += '\nexport const '+ brandName +' = \`';
+              cssContent = ''
+              cssContent += '\nexport const ' + brandName + ' = `'
 
-              cssContent += dt[a];
-              cssContent += '\`\n';
-              if(componentCSS[filename]) componentCSS[filename] += cssContent;
-              else componentCSS[filename] = cssContent;
+              cssContent += dt[a]
+              cssContent += '`\n'
+              if (componentCSS[filename]) componentCSS[filename] += cssContent
+              else componentCSS[filename] = cssContent
             }
-            data[file] = dt;
-            if (!--pending) done(null, data, globalCSS, componentCSS);
-          });
-        } else {
-          fs.readFile(file, 'utf-8', function(err, content){
-            if(err) console.log(err);
-            data[file] = content;
-            if (!--pending) done(null, data);
+            data[file] = dt
+            if (!--pending) done(null, data, globalCSS, componentCSS)
           })
-
+        } else {
+          fs.readFile(file, 'utf-8', function(err, content) {
+            if (err) console.log(err)
+            data[file] = content
+            if (!--pending) done(null, data)
+          })
         }
-      });
-    });
-  });
-};
-
-const components = 'src/components/';
-const inputFolder = 'src/themes/';
-const outputFolder = 'src/themes.built/';
-
-var counter = 0,
-    addString = '';
-
-if (!fs.existsSync(outputFolder)){
-  fs.mkdirSync(outputFolder);
-}
-
-fs.readdir(components, (err, files) => {
-  if(err) console.log(err);
-  files.forEach(file => {
-    walkDir(inputFolder, function(err, data, globalCSS, componentCSS){
-      console.log('============ checking ', file);
-
-      if (err) console.log(err);
-      for(var key in componentCSS){
-        if(file === key){
-          addString = '';
-          addString += componentCSS[key]
-          fs.writeFile(outputFolder + file + '.ts', addString, 'utf8', (err) => {
-            if (err) throw err;
-            console.log('write file ' + file + '.ts');
-          });
-        }
-      }
+      })
     })
   })
-})
+}
+
+function generateTheme() {
+  var counter = 0,
+    addString = ''
+
+  if (!fs.existsSync(outputFolder)) {
+    fs.mkdirSync(outputFolder)
+  }
+
+  fs.readdir(components, (err, files) => {
+    if (err) console.log(err)
+    files.forEach(file => {
+      walkDir(inputFolder, function(err, data, globalCSS, componentCSS) {
+        console.log('============ checking ', file)
+
+        if (err) console.log(err)
+        for (var key in componentCSS) {
+          if (file === key) {
+            addString = ''
+            addString += componentCSS[key]
+            fs.writeFile(
+              outputFolder + file + '.ts',
+              addString,
+              'utf8',
+              err => {
+                if (err) throw err
+                console.log('write file ' + file + '.ts')
+              }
+            )
+          }
+        }
+      })
+    })
+  })
+}
+
+generateTheme()
+
+console.log(process.argv)
+
+if (process.argv.indexOf('--watch') > -1) {
+  console.log('Watching themes for changes.')
+
+  fs.watch(inputFolder, (eventType, filename) => {
+    generateTheme()
+  })
+}

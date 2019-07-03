599b6567c20dbd53c3859453256735b8439fbb93
Andreas Wikström <andreas.wikstrom@scania.com>
2019-07-03 14:57:27 +0200

    Changed initiation of icon component - Changed rendering of the icon component from slot to host

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -1,5 +1,5 @@
 import {
-  Component, Prop, State, Watch,
+  Component, Prop, State, Watch, Element,
 } from '@stencil/core';
 
 import { library, findIconDefinition, icon } from '@fortawesome/fontawesome-svg-core';
@@ -18,27 +18,20 @@ export class Icon {
 
   @State() icon: any;
 
+  @Element() el: any;
+
   @Watch('name')
   setIcon(name) {
     let iconObject = findIconDefinition({ prefix: 'fas', iconName: name });
     iconObject = iconObject || findIconDefinition({ prefix: 'fab', iconName: name });
     iconObject = iconObject || findIconDefinition({ prefix: 'fas', iconName: 'question' });
     if (iconObject) {
-      this.icon = icon(iconObject).html;
+      this.icon = icon(iconObject).node[0];
+      (this.el.shadowRoot || this.el).appendChild(this.icon);
     }
   }
 
   componentWillLoad() {
     this.setIcon(this.name);
-  }
-
-  hostData() {
-    return {
-      innerHTML: this.icon,
-    };
-  }
-
-  render() {
-    return <slot />;
   }
 }

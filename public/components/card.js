b82a82fb9bb478e38c31319f03fb434ef7a8e007
Marcus Mena <marcus.mena.pacheco@scania.com>
2019-10-02 13:27:02 +0200

    Update bootstrap preview cards

--- public/components/card.js
+++ public/components/card.js
@@ -88,29 +88,29 @@ export default {
     //   </div>
     //   `,
     // },
-    {
-      title: 'List group inside',
-      content: `
-      <div class="card" style="width: 30rem;">
-        <svg class="card-img-top">
-          <rect width="100%" height="150px" fill="#868e96"></rect>
-        </svg>
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">Cras justo odio</li>
-          <li class="list-group-item">Dapibus ac facilisis in</li>
-          <li class="list-group-item">Vestibulum at eros</li>
-        </ul>
-        <div class="card-body">
-          <a href="#" class="card-link">Card link</a>
-          <a href="#" class="card-link">Another link</a>
-        </div>
-      </div>
-      `,
-    },
+    // {
+    //   title: 'Kitchen sink',
+    //   content: `
+    //   <div class="card" style="width: 30rem;">
+    //     <svg class="card-img-top">
+    //       <rect width="100%" height="150px" fill="#868e96"></rect>
+    //     </svg>
+    //     <div class="card-body">
+    //       <h5 class="card-title">Card title</h5>
+    //       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    //     </div>
+    //     <ul class="list-group list-group-flush">
+    //       <li class="list-group-item">Cras justo odio</li>
+    //       <li class="list-group-item">Dapibus ac facilisis in</li>
+    //       <li class="list-group-item">Vestibulum at eros</li>
+    //     </ul>
+    //     <div class="card-body">
+    //       <a href="#" class="card-link">Card link</a>
+    //       <a href="#" class="card-link">Another link</a>
+    //     </div>
+    //   </div>
+    //   `,
+    // },
     // {
     //   title: 'Header',
     //   content: `
